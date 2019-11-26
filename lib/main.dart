2e40133a203936add7b3a1f015c3fb065941745a
wal33d006 <waleed_arshad006@hotmail.com>
2019-11-26 22:00:10 +0500

    Add basic UI for reminder application

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -25,19 +27,197 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+      backgroundColor: Colors.black,
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            ListTile(
+              title: SizedBox(
+                height: 35.0,
+                child: Theme(
+                  data: ThemeData(
+                      hintColor: Colors.transparent,
+                      primaryColor: Colors.transparent),
+                  child: TextFormField(
+                    cursorColor: Colors.white38,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.mic,
+                        color: Colors.white38,
+                      ),
+                      fillColor: Colors.white10,
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white38,
+                      ),
+                      hintStyle: TextStyle(color: Colors.white38),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                CircleAvatar(
+                                  radius: 15.0,
+                                  child: Icon(
+                                    Icons.calendar_today,
+                                    size: 20.0,
+                                  ),
+                                  backgroundColor: Colors.blue,
+                                  foregroundColor: Colors.white70,
+                                ),
+                                Text(
+                                  '0',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .display1
+                                      .copyWith(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Text(
+                                'Today',
+                                style:
+                                    Theme.of(context).textTheme.subtitle.copyWith(
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      color: Colors.white10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  SizedBox(width: 6.0,),
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                CircleAvatar(
+                                  radius: 15.0,
+                                  child: Icon(
+                                    Icons.timelapse,
+                                    size: 22.0,
+                                  ),
+                                  backgroundColor: Colors.orange,
+                                  foregroundColor: Colors.white,
+                                ),
+                                Text(
+                                  '0',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .display1
+                                      .copyWith(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Text(
+                                'Scheduled',
+                                style:
+                                Theme.of(context).textTheme.subtitle.copyWith(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      color: Colors.white10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                CircleAvatar(
+                                  radius: 15.0,
+                                  child: Icon(
+                                    Icons.archive,
+                                    size: 22.0,
+                                  ),
+                                  backgroundColor: Colors.white38,
+                                  foregroundColor: Colors.white,
+                                ),
+                                Text(
+                                  '23',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .display1
+                                      .copyWith(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Text(
+                                'All',
+                                style:
+                                Theme.of(context).textTheme.subtitle.copyWith(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      color: Colors.white10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
